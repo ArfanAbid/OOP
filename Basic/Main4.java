b2fc7a976612c4a85eaa24c71619686f60e54fc6
Arfan Abid <131770875+ArfanAbid@users.noreply.github.com>
2023-09-08 18:06:19 +0500

    Create Main4.java

---
+++ Basic/Main4.java
@@ -0,0 +1,47 @@
+class Car{
+  String color;
+  int model;
+  int price;
+  // constructor example:
+   Car(){
+  System.out.println("This is Constructor ");
+
+  }
+  
+  
+   // Overloading exampless:
+  
+  Car(String color,int model){
+  System.out.println("Overloading example :");
+
+  System.out.println("Colour is :" +color);
+  System.out.println("Model is :" +model);    
+  System.out.println("Call for price");    
+
+  }
+  Car(String color,int model,int  price){
+  System.out.println("Overloading example :");
+    
+  System.out.println("Colour is :" +color);
+  System.out.println("Model is :" +model);    
+  System.out.println("Price is :" +price);    
+
+  }
+
+ 
+}
+
+
+class Main4 {
+  public static void main(String[] args) {
+    Car c1=new Car();
+    Car c2=new Car("Silver",16,240000);
+    Car c3=new Car("Silver",16);
+   
+
+
+    
+    
+    
+  }
+}
