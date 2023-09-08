5bbfcc0cffaebd3c3e935167fec98d362497b1b4
Arfan Abid <131770875+ArfanAbid@users.noreply.github.com>
2023-09-08 18:15:49 +0500

    Create Main.java

---
+++ Constructors/Main.java
@@ -0,0 +1,74 @@
+ public class Main {
+  public static void main(String[] args) {
+  //Constructor Concepts
+// public class Constructor {
+//     // All about constructor
+//     // --> Default constructor write by Compiler whether you write or not
+//     // --->Parameterized constructor write by Programmer
+//     // --->Default constructor write by Programmer
+//     // Starting ...
+//     public static void main(String[] args) {
+//       BankAccount moiz = new BankAccount("Moiz");
+//       // now here constructor works take argument at the time of creation of object
+//       moiz.balance(120);
+//       moiz.print();
+//     }
+  
+//     static class BankAccount {
+//       private String Accountname;
+//       private double balance;
+  
+//       // Constructor
+//       public BankAccount(String accountname) {
+//         this.Accountname = accountname;
+//       }
+  
+//       public void balance(double amount) {
+//         this.balance = amount;
+//       }
+  
+//       public void print() {
+//         System.out.println("The Owner name is " + Accountname);
+//         System.out.println("The Balance is " + balance);
+//       }
+//     }
+//   }
+
+    //-------------------------------
+  // Visbilty Quantifiers    Example Program
+//     public class MyClass {
+//     static int myStaticVariable; // static member
+
+//     int myNonStaticVariable; // non-static member
+
+//     public static void myStaticMethod() {
+//         System.out.println("This is a static method!");
+//     }
+
+//     public void myNonStaticMethod() {
+//         System.out.println("This is a non-static method!");
+//     }
+
+//     public static void main(String[] args) {
+//         MyClass myObject1 = new MyClass();
+//         MyClass myObject2 = new MyClass();
+
+//         // Accessing static member
+//         MyClass.myStaticVariable = 10;
+//         System.out.println("Static variable value: " + MyClass.myStaticVariable);
+
+//         MyClass.myStaticMethod(); // calling static method
+
+//         // Accessing non-static member
+//         myObject1.myNonStaticVariable = 20;
+//         myObject2.myNonStaticVariable = 30;
+//         System.out.println("Non-static variable values: " + myObject1.myNonStaticVariable + ", " + myObject2.myNonStaticVariable);
+
+//         myObject1.myNonStaticMethod(); // calling non-static method
+//         myObject2.myNonStaticMethod(); // calling non-static method
+//     }
+// }
+
+    
+  }
+}
