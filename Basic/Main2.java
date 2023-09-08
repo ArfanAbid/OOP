a8eac2fcb82a3c257168a59129b2f7a3ba2f0a9f
Arfan Abid <131770875+ArfanAbid@users.noreply.github.com>
2023-09-08 18:00:54 +0500

    Create Main2.java

---
+++ Basic/Main2.java
@@ -0,0 +1,32 @@
+class Main2 {
+  int x=5;
+    int y=6;
+    int z=7;
+
+
+
+  public static void main(String[] args) {
+    Main o1=new Main();
+        Main o2=new Main();
+      Main o3=new Main();
+  
+                                          
+    System.out.println("The value of o1 is"+o1.x);
+    System.out.println("The value of o1 is"+o1.x);
+    System.out.println("The value of o1 is"+o1.x);
+
+    System.out.println("The value of o2 is"+o2.y);
+    System.out.println("The value of o2 is"+o2.y);
+    System.out.println("The value of o2 is"+o2.y);
+
+    System.out.println("The value of o3 is"+o3.z);
+    System.out.println("The value of o3 is"+o3.z);
+    System.out.println("The value of o3 is"+o3.z);
+
+
+
+
+
+
+  }
+}
