af14db67f568b9ee87cfaf6e094e5609e1a6f693
Arfan Abid <131770875+ArfanAbid@users.noreply.github.com>
2023-09-08 17:58:48 +0500

    Create Main.java

---
+++ Basic/Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Arfan Abid!");
+
+    int a=5;
+    int b =2;
+    int c=a+b;
+    System.out.println("The Sum of function is:");
+    System.out.println(c);
+  
+    
+  }
+}
