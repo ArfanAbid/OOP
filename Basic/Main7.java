886bd4e64e83a3cda81509395ad957d58f781b53
Arfan Abid <131770875+ArfanAbid@users.noreply.github.com>
2023-09-08 18:20:44 +0500

    Create Main7.java

---
+++ Basic/Main7.java
@@ -0,0 +1,15 @@
+public class Main7{
+    int a=10;
+    static int b=20;
+    public static void main(String[] args){
+    Main7 r=new Main7();
+    r.Disp();
+    Main.Show();
+          }
+static void Show(){
+    System.out.println("Show " +b);
+   }
+void Disp(){
+    System.out.println("Disp "+a+" "+b);
+}
+}
