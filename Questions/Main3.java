0992da91b88c22ae174a2dc1c34c4f0a65d2c616
Arfan Abid <131770875+ArfanAbid@users.noreply.github.com>
2023-09-08 19:20:19 +0500

    Create Main3.java

---
+++ Questions/Main3.java
@@ -0,0 +1,11 @@
+// Implement the concept of Date n java
+
+import java.util.Date;
+public class Main3 {
+    public static void main(String args[]) {
+       
+       Date  date = new Date();
+
+       System.out.println(date);
+   }
+}
