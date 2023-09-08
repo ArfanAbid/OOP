c5d52f79e30f1c4f6e86257cbd259ed71abb68cd
Arfan Abid <131770875+ArfanAbid@users.noreply.github.com>
2023-09-08 19:02:18 +0500

    Create Main.java

---
+++ Array&ArrayList/Main.java
@@ -0,0 +1,17 @@
+class Array{
+  int[] arr=new int[5];
+  String[] str;
+  Array(){
+    System.out.println("Implementing the concept of Arrays ");
+   // arr[0]=5;
+  }
+ 
+}
+class Main{
+  public static void main(String[] args){
+    
+    Array a=new Array();
+    
+    System.out.println(a.arr[0]);
+  }
+}
